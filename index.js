e99976ea6654c8927a4ef998f374d0593dfec440
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2021-11-14 21:36:31 +0200

    ex 3.19

--- index.js
+++ index.js
@@ -59,27 +59,24 @@ app.delete("/api/persons/:id", (request, response, next) => {
 
 })
 
-app.post("/api/persons", async (request, response) => {
+app.post("/api/persons", async (request, response, next) => {
   const body = await request.body
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: "name or number missing"
-    })
-  }
-  const nameAlreadyTaken = await Person.find({}).then(people => people.find(person => person.name === body.name))
-  if (nameAlreadyTaken) {
-    return response.status(400).json({ 
-      error: "name must be unique"
-    })
-  }
+  // const nameAlreadyTaken = await Person.find({}).then(people => people.find(person => person.name === body.name))
+  // if (nameAlreadyTaken) {
+  //   return response.status(400).json({ 
+  //     error: "name must be unique"
+  //   })
+  // }
 
   const person = new Person({
     name: body.name,
     number: body.number
   })
 
-  person.save().then(savedPerson => response.json(savedPerson))
+  person.save()
+  .then(savedPerson => response.json(savedPerson))
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -100,7 +97,13 @@ const unknownEndpoint = (request, response) => {
 }
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
-  if (error.name === 'CastError') return response.status(400).send({ error: 'malformatted id' })
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
+
   next(error)
 }
 
